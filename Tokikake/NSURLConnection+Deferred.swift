406ff748032893bae2bf92cbaab4398a2d6aea24
yukiasai <yukiasai@gmail.com>
2014-12-30 23:51:07 +0900

    Add load UIImage in NSURLConnection+Deferred

--- Tokikake/NSURLConnection+Deferred.swift
+++ Tokikake/NSURLConnection+Deferred.swift
@@ -29,5 +29,22 @@ extension NSURLConnection {
         }
         return deferred.promise
     }
-    
+	
+	public class func request(url: String) -> Promise<UIImage, NSError, Float> {
+		let deferred = Deferred<UIImage, NSError, Float>()
+		
+		let request = NSMutableURLRequest()
+		request.URL = NSURL(string: url)!
+		NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+			if let error = error {
+				deferred.reject(error)
+				return
+			}
+			
+			let image = UIImage(data: data!)
+			deferred.resolve(image!)
+		}
+		return deferred.promise
+	}
+	
 }
